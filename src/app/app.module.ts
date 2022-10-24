963b94c834167f9e8d58d95cb6729fe22155bf8e
Daniel Cuesta <dcuestas@sia.es>
2022-10-24 16:49:33 +0200

    Modules 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
+
+import { ContadorModule } from './contador/contador.module';
 
 import { HeroesModule } from './heroes/heroes.module';
 
@@ -10,11 +11,11 @@ import { HeroesModule } from './heroes/heroes.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
